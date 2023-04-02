4fb486c9f3a23ccd750fdd7571c4371c1d0e88cd
Sanyam <sanyamjha19@gmail.com>
2023-04-02 21:58:53 +0530

    Add files via upload

--- dijkstra.py
+++ dijkstra.py
@@ -5,62 +5,105 @@ from node import *
 net = network(100, 100, 40, 0, 0)
 net.set_parameters(2000,2000,2000,2000, 30)
 net.initialise_nodes(1, 0.2)
-net.show_network()
-sink = [net.base_station_x, net.base_station_y]
+
+# net.show_network()
+sink = net.sink
 operational_nodes = net.number_of_nodes
 
 #	operational log
 dead_nodes = []
-rnds = 0
+rnd = 0
 operational_log = []
 
-net.network_adj_matrix()
-
-# 	n_list = adj[Node.id]
-# 	for Node in net.node_list:
-# 	print(Node.id, end  = ": ")
-# 	for x in n_list:
-# 		print(f"({x[0].id}), ({x[1]})", end = ", ")
-# 	print()
-
-
-shortest_paths = net.dijkstra()
-
-prev_round_operational_nodes = operational_nodes
-
+path = net.dijkstra()
+print(path)
+packets_sent = 0
+packets_received = 0
+packet_length = net.packet_length
+dead_nodes = set()
 while operational_nodes > 0:
-	rnds += 1
+	rnd += 1
+	packet_to_sink = 0
 
-	if(prev_round_operational_nodes != operational_nodes):
-		prev_round_operational_nodes = operational_nodes
-		shortest_paths = net.dijkstra()
+	### 	Stable------
+	
+	### Latest---
+	for i in range(1, net.number_of_nodes + 1):
+		if i in dead_nodes:
+			continue
+		curr_id = i
+		next_id = path[curr_id][0]
+		while next_id != 0:
+			currNode = net.node_map[curr_id]
+			nextNode = net.node_map[next_id]
+			if next_id in dead_nodes:
+				next_id = 0
+				break
+			res = currNode.transmit(packet_length, path[curr_id][1], nextNode)
+			curr_id = 	next_id
+			next_id =	path[curr_id][0]
 
-	for Node in net.node_list:
-		path = shortest_paths[Node]
-		Node.current_energy -= Node.energy_for_transmission(net.packet_length,
-						      Node.dist_from_node(path[0]))
-
-		currNode = path.pop(0)
-		while(len(path) != 0):
-			currNode.current_energy -= currNode.energy_for_reception(net.packet_length)
-			currNode.current_energy -= currNode.energy_for_transmission(net.packet_length,
-							       currNode.dist_from_node(path[0]))
-
-			if(currNode.critical_energy > currNode.current_energy):
-				operational_nodes -= 1
+		#	if the next node is sink node:
+		currNode = net.node_map[curr_id]
+		res = currNode.transmit(packet_length, currNode.dist(sink), sink)
+		if(res == -1):
+			operational_nodes -= 1
+			dead_nodes.add(curr_id)
+		else:
+			packet_to_sink += 1
 
 
+	packets_received += packet_to_sink
+	print(rnd, packets_received, operational_nodes)
+	print("----Energy_each_node----")
+	for i in net.node_list:
+		print(i.current_energy)
+
+
+print(f"number of rounds: {rnd}")
 
 
 
-#	Average Path Length
-# APL = 0
-# for i in dp:
-# 	APL += i
+### Stable---
 
-# APL /= net.number_of_nodes
-# APL = round(APL, 2)
-# print(APL)
-# dp = sorted(dp, reverse = True)
-# print(dp)
+# packets = [1 for i in range(net.number_of_nodes + 1)]
+	# packets[0] = 0
 
+	# for i in range(1, net.number_of_nodes + 1):
+	# 	Node = net.node_map[i]
+
+	# 	if Node.is_functional() == False:
+	# 		operational_nodes -= 1
+	# 		continue
+
+	# 	if(path[i] == -1):
+	# 		receiver_node = sink
+	# 		d = Node.dist(sink)
+	# 	else:
+	# 		receiver_node = net.node_map[path[i][0]]
+	# 		d = path[i][1]
+
+	# 	# print("transsmission info", i, receiver_node.id, d)
+	# 	while packets[i] != 0:
+	# 		if Node.critical_energy > Node.current_energy: #Node not functional
+	# 			operational_nodes -= 1
+	# 			break
+
+	# 		elif receiver_node.critical_energy > receiver_node.current_energy: #Receiver not functional
+	# 			res = Node.transmit(net.packet_length, Node.dist(sink), sink)
+	# 			if res == 0:
+	# 				break
+	# 			else:
+	# 				packets[i] -= 1
+	# 				packets[0] += 1
+
+	# 		else:	#Both are functional
+	# 			res = Node.transmit(net.packet_length, Node.dist(receiver_node), receiver_node)
+	# 			if res == 0:
+	# 				break
+
+	# 			packets[i] -=1
+	# 			packets[receiver_node.id] += 1
+
+	# 	if Node.is_functional() == False:
+	# 		operational_nodes -=1
